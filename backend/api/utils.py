dc3cfe95f59170771c4ac0d32e1cb67e17fdaad0
Tinashe Kucherera <tkucherera86@gmail.com>
2026-04-11 01:56:28 -0400

    fix uid issue

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,10 +1,9 @@
 import os
 
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from allauth.account.utils import user_pk_to_url_str
 
 
 def custom_password_reset_url_generator(request, user, temp_key):
-    frontend_url = os.getenv("FRONTEND_URL", "http://localhost").rstrip("/")
-    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    frontend_url = os.getenv("FRONTEND_URL", "http://localhost:5173").rstrip("/")
+    uid = user_pk_to_url_str(user)
     return f"{frontend_url}/password-reset/confirm/{uid}/{temp_key}"
